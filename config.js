383974d1df8d86e27d5ad9a616176d421e79dc45
MarkPedersen <markpedersen1991@hotmail.com>
2016-06-13 21:30:40 +0200

    config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	'port': process.env.PORT || 8080,
-	'database': 'mongodb://localhost/badminton',
+	'database': 'mongodb://node:noder@novus.modulusmongo.net:27017/Iganiq8o',
 	'secret': 'ilovescotchscotchyscotchscotch'
 };
